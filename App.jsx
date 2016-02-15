44918ac9857a2579cc77e26a270c3c6cd20cebc9
John Domingo <johnwill.domingo@gmail.com>
2016-02-15 15:32:34 -0800

    Add owner and username fields to Tasks collection

--- App.jsx
+++ App.jsx
@@ -38,7 +38,9 @@ App = React.createClass({
 
     Tasks.insert({
       text: text,
-      createdAt: new Date() // current time
+      createdAt: new Date(),
+      owner: Meteor.userId(),           // _id of logged in user
+      username: Meteor.user().username  // username of logged in user
     });
 
     // Clear form
