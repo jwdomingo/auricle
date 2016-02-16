73f5835f5d682c3039b9d5490c8a1fa70ea75c95
John Domingo <johnwill.domingo@gmail.com>
2016-02-16 15:04:23 -0800

    Implement getMedia SoundCloud API request and createChannel mongoDB insert method

--- auricle.jsx
+++ auricle.jsx
@@ -1,5 +1,6 @@
 Messages = new Mongo.Collection("messages");
 Channels = new Mongo.Collection("channels");
+Media = new Mongo.Collection("media");
 
 if (Meteor.isClient) {
   Accounts.ui.config({ passwordSignupFields: "USERNAME_ONLY" });
@@ -7,6 +8,7 @@ if (Meteor.isClient) {
 
   Meteor.subscribe("messages");
   Meteor.subscribe("channels");
+  Meteor.subscribe("media");
 
   Meteor.startup(function () {
     ReactDOM.render(<App />, document.getElementById("render-target"));
@@ -24,16 +26,63 @@ if (Meteor.isServer) {
   });
 
   Meteor.publish("channels", function () {
-    return Channels.find({
-      owner: this.userId
-    });
+    return Channels.find({});
+  });
+
+  Meteor.publish("media", function () {
+    return Media.find({});
   });
 }
 
 Meteor.methods({
+  createChannel(channelName, content, isPrivate) {
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("Not authorized to create new channels");
+    }
+    Channels.insert({
+      channel: channelName,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
+      media: content,
+      private: isPrivate
+    });
+  },
+
+  getMedia(channel) {
+    if (!Meteor.userId()) {
+      throw new Meteor.Error("Not authorized to fetch new media");
+    }
+
+    var url = 'https://api.soundcloud.com/tracks/?q=beyonce&license=cc-by-sa&client_id=' + Meteor.settings.public.soundcloudApiKey;
+
+    return HTTP.getPromise(url)
+      .then(function(response) {
+        return response.data;
+      })
+      .catch(function(error) {
+        if (error) {
+          console.error('Server side error', error);
+        }
+      });
+  },
+
+  createMedia(content) {
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("Not authorized to create new media");
+    }
+    Media.insert({
+      title: content.title,
+      creator: content.creator,
+      type: content.type,
+      thumb: content.thumb,
+      source: content.source
+    });
+  },
+
   addMessage(text, channel) {
     if (! Meteor.userId()) {
-      throw new Meteor.Error("not-authorized");
+      throw new Meteor.Error("Not authorized to chat");
     }
     Messages.insert({
       content: text,
@@ -47,7 +96,7 @@ Meteor.methods({
   removeMessage(msgId) {
     const message = Messages.findOne(msgId);
     if (message.owner !== Meteor.userId()) {
-      throw new Meteor.Error("not-authorized");
+      throw new Meteor.Error("Not authorized to delete messages");
     }
     Messages.remove(msgId);
   },
